7248357036c5d048cf70fc4304cad8c26a8b4798
abrar0786 <theibrar1000@gmail.com>
2023-06-17 23:14:01 +0500

    added funtionality of persist the user clicked image

--- src/screens/VisionCamera/VisionCamera.js
+++ src/screens/VisionCamera/VisionCamera.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
@@ -7,46 +8,70 @@ import {
   Image,
 } from 'react-native';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import AppText from '../../components/UI/AppText';
+import mycolors from '../../styles/mycolors';
 
 const VisionCamera = () => {
+  const LOCAL_ST = 'sst989';
+  let ITEM = null;
+  const photoCollection = [];
   const cammeraRef = useRef(null);
 
   const devices = useCameraDevices();
   const device = devices.back;
   const [imagePath, setimagePath] = useState(null);
+  const [storedImage, setstoredImage] = useState(null);
+
   // console.log('devices__:', device);
 
   useEffect(() => {
     persmissionCmera();
   }, []);
 
+  const getItem = async () => {
+    try {
+      ITEM = await AsyncStorage.getItem(LOCAL_ST);
+      setstoredImage(ITEM);
+      const id = Math.floor(Math.random() * 100);
+      photoCollection.push({id: id, pic: `file://${ITEM}`});
+    } catch (error) {}
+  };
+
+  getItem();
+  console.log('photoCollection__:\n\n', photoCollection);
+
+  const photoClick = async () => {
+    const photo = await cammeraRef.current.takePhoto();
+    console.log('photo___:\n\n\n', photo.path);
+    AsyncStorage.setItem(LOCAL_ST, photo.path);
+    setimagePath(photo.path);
+  };
+
+  const clickAgain = () => {
+    setimagePath(null);
+    const nullPic = AsyncStorage.removeItem(LOCAL_ST);
+    setstoredImage(nullPic);
+  };
+
   const persmissionCmera = async () => {
     try {
       const cameraPermission = await Camera.getCameraPermissionStatus();
       const microphonePermission = await Camera.getMicrophonePermissionStatus();
       const devices = await Camera.getAvailableCameraDevices();
-      console.log('devices__234:\n\n\n', devices.hasTorch);
+      // console.log('devices__234:\n\n\n', devices);
     } catch (error) {
       console.log('erorr__:', error);
     }
-  };
-
-  const photoClick = async () => {
-    const photo = await cammeraRef.current.takePhoto();
-    console.log('photo___:\n\n\n', photo.path);
-    setimagePath(photo.path);
-  };
-  const clickAgain = () => {
-    setimagePath(null);
   };
   return (
     <React.Fragment>
       {device ? (
         <React.Fragment>
-          {imagePath ? (
+          {storedImage ? (
             <>
               <Image
-                source={{uri: `file://${imagePath}`}}
+                // source={{uri: `file://${imagePath}`}}
+                source={{uri: `file://${storedImage}`}}
                 style={{
                   width: '100%',
                   height: '100%',
@@ -56,9 +81,11 @@ const VisionCamera = () => {
               />
               <View style={styles.captureButtonContainer}>
                 <TouchableOpacity
-                  style={styles.captureButton}
-                  onPress={clickAgain}
-                />
+                  style={{
+                    ...styles.captureButton,
+                    backgroundColor: mycolors.red,
+                  }}
+                  onPress={clickAgain}></TouchableOpacity>
               </View>
             </>
           ) : (
